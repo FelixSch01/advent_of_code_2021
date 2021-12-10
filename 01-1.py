78c8e363c7e1f7d08fbafa47b368e4650fe232f2
Felix Schmölzer <felix.schmoelzer1@gmail.com>
2021-12-10 18:30:25 +0100

    add argument handling and read file

--- 01-1.py
+++ 01-1.py
@@ -1,1 +1,16 @@
 #!/bin/python3
+ 
+# --- Day 1: Sonar Sweep ---
+# https://adventofcode.com/2021/day/1
+
+# imports
+import sys as s
+
+# handle arguments
+if len(s.argv[1:]) == 1:
+    file = s.argv[1]
+else:
+    print('usage: 01-1.py <input file>')
+
+# read input file
+depth_measurements = file.read()
