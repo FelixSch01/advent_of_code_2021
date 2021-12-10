843c6c41595c9fdc7cda148f0f0c62e5fdfb3462
Felix Schmölzer <felix.schmoelzer1@gmail.com>
2021-12-11 00:54:15 +0100

    restructure code to be more modular and to accomodate for 'part two'

--- 01.py
+++ 01.py
@@ -13,21 +13,30 @@ else:
     print('usage: 01-1.py <input file>')
     sys.exit(-1)
 
-# read input file
-depth_measurements = file.readlines()
+# read input file and convert to int array
+depth_measurements = []
+for line in file.readlines():
+    depth_measurements.append(int(line.rstrip()))
 
-# Loop over numbers to calculate the amount of times a number is bigger
-# compared to the previous number. The first number is excluded.
-i = 0
-result = 0
-while(i < len(depth_measurements)):
-    depth_measurements[i] = int(depth_measurements[i].rstrip())
-    if i == 0:
+# close file
+file.close()
+
+def part_one(numbers):
+    # Loop over numbers to calculate the amount of times a number is bigger
+    # compared to the previous number. The first number is excluded.
+    i = 0
+    result = 0
+    while(i < len(numbers)):
+        if i == 0:
+            i += 1
+            continue
+        if numbers[i] > numbers[i - 1]:
+            result += 1
         i += 1
-        continue
-    if depth_measurements[i] > depth_measurements[i - 1]:
-        result += 1
-    i += 1
+    return result
 
-# display the result
-print(result)
+def part_two():
+    pass
+
+# display the results
+print(part_one(depth_measurements))
