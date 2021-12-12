f7ce1047e7394e7aadc97ed1b9f46bf0cbdd18a6
Felix Schmölzer <felix.schmoelzer1@gmail.com>
2021-12-12 01:53:23 +0100

    implement 'part one'

--- 02.py
+++ 02.py
@@ -14,11 +14,34 @@ else:
     sys.exit(-1)
 
 # read input file and convert to int array
-commands = []
+steering = []
 for line in file.readlines():
-    temp = [line.split(" ")]
+    temp = line.split(" ")
     temp[1] = int(temp[1].rstrip())
-    commands.append(temp)
+    steering.append(temp)
 
 # close file
 file.close()
+
+# Loop over commands and compute final horizontal position and depth.
+# Horizontal position and depth each start at 0, the command 'forward'
+# increases the horizontal position by its value, the command 'down'
+# increases and the command 'up' decreases depth by its value.
+# The final positions get multiplied to compute the desired value.
+def part_one(commands):
+    horizontal_pos = 0
+    depth = 0
+    for command in commands:
+        if command[0] == "forward":
+            horizontal_pos += command[1]
+        elif command[0] == "down":
+            depth += command[1]
+        elif command[0] == "up":
+            depth -= command[1]
+    return horizontal_pos * depth
+
+# display the results
+print("result for part one:    ", part_one(steering))
+
+# exit the program
+sys.exit(0)
